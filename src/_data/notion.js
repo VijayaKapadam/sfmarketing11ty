b52ec2ba75c89c3c33bd86bacc6eee0f5246808c
Vijaya Kapadam <vijayraghava.k@gmail.com>
2023-08-10 15:04:10 +1000

    new chanegs with notion

--- src/_data/notion.js
+++ src/_data/notion.js
@@ -35,7 +35,7 @@ exports.handler = async function (event, context) {
         sorts: [
           {
             property: 'Published Date',
-            direction: 'ascending',
+            direction: 'descending',
           },
         ]
     });
